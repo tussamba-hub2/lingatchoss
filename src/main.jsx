629048c71316ad88371be41af95d872710ec41a3
nap-uige <nacionalajudaopais@gmail.com>
2025-08-12 10:24:35 +0100

    Third commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './i18n.js'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>,
 )

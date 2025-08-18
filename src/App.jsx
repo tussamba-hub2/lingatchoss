f543366c078441409d79459f13c39ddbb5839a61
tussamba-hub2 <tussambadomingosnfutila@gmail.com>
2025-08-18 15:34:56 +0100

    You khow

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Authentication from "./auth/Authentication";
 import Dashboard from "./panel/Dashboard";
 import InstitutionDetails from "./details/InstitutionDetails";
 import EditService from "./components/edit/Service.jsx";
 import Home from "./global/Home.jsx";
-import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/lingatchoss">
-      <Routes>
-        <Route path="/sign-in" element={<Authentication />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/details" element={<InstitutionDetails />} />
-        <Route path="/edit-service/:serviceId" element={<EditService />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/sign-in" element={<Authentication />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/details" element={<InstitutionDetails />} />
+      <Route path="/edit-service/:serviceId" element={<EditService />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 

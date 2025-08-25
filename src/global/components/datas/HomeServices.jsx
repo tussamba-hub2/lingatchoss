881a6145eb1f288ddf474183864f04088811151c
tussamba-hub2 <tussambadomingosnfutila@gmail.com>
2025-08-25 11:11:31 +0100

    25/08 morning commit 7

--- src/global/components/datas/HomeServices.jsx
+++ src/global/components/datas/HomeServices.jsx
@@ -220,8 +220,8 @@ export default function HomeServices() {
 
   return (
     <section className="show-products-home">
-      <div className="d-flex column g-32px w-full rs-p-16">
-        <div className="d-flex items-center justify-between">
+      <div className="d-flex column g-32px w-full">
+        <div className="d-flex items-center justify-between rs-p-16">
           <h3 className="size-20 extra-bold">
             {t("discover_service_categories")}
           </h3>
